03d4760c038319ab4984f06a9f2279a371e1bbab
artur-calderon <artur.calderonart@gmail.com>
2026-07-18 15:15:01 -0300

    url render

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ import errorHandler from "./middlewares/errorHandler.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Needed on Render so https is detected correctly
+app.set("trust proxy", 1);
+
 const swaggerDocument = JSON.parse(readFileSync("./swagger.json", "utf8"));
 
 app.use(express.json());
@@ -19,24 +22,7 @@ app.get("/", (req, res) => {
 	res.send("Todo API is running. Go to /api-docs to see the documentation.");
 });
 
-app.use(
-	"/api-docs",
-	(req, res, next) => {
-		const protocol = req.headers["x-forwarded-proto"] || req.protocol;
-		const host = req.get("host");
-
-		swaggerDocument.servers = [
-			{
-				url: process.env.BASE_URL || `${protocol}://${host}`,
-				description: "API server",
-			},
-		];
-
-		next();
-	},
-	swaggerUi.serve,
-	swaggerUi.setup(swaggerDocument),
-);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/", routes);
 
